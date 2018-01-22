19902ad857093fcece79c87cf2307b181722a90e
alisam1 <alisa-m2@yandex.ru>
2018-01-22 23:43:15 +0300

    commit

--- js/tabs.js
+++ js/tabs.js
@@ -3,7 +3,7 @@ $(function() {
 
     $('ul.tabs-list').delegate('li:not(.tabs-list__item-current)', 'click', function() {
         $(this).addClass('tabs-list__item-current').siblings().removeClass('tabs-list__item-current')
-            .parents('div.section').find('div.box').hide().eq($(this).index()).fadeIn(150);
+            .parents('div.section').find('div.box').hide().eq($(this).index()).fadeIn(200);
     })
 
 })
